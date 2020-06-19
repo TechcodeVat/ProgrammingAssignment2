a9aa886f54170be7e026f4692d624001054f3c4e
TechcodeVat <67142370+TechcodeVat@users.noreply.github.com>
2020-06-19 13:28:30 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes in the matrix as input and gives the inverse of it as output
+## If the matrix is repeated it code uses the cache memory to extract the inverse hence saving time.
 
-## Write a short comment describing this function
+## Takes matrix and gives inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+            inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+
 
 }
 
 
-## Write a short comment describing this function
+## Checks if the matrix is repeated or not; 
+##if repeated then pulls the inverse from cache memory or calls the earlier function to give inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached result")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
+
 }
